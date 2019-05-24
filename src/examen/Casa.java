3e96d814582c38bf2bc01a57753490427040aeb8
Aitor <48829781+Lasur98@users.noreply.github.com>
2019-05-24 14:01:30 +0200

    Actualizado el archivo Casa.java
    Se ha actualizado el metodo limpiar

--- src/examen/Casa.java
+++ src/examen/Casa.java
@@ -7,12 +7,12 @@ public class Casa {
 	private int codigoPostal;
 	
 	/**
-	 * Un equipo de limpiadores deja la cas alibre de germenes
-	 * y desinfecta todas las superficies
+	 * Un equipo de ladrones deja la casa limpa 
+	 * y roba hasta el cobre de las paredes
 	 */
 	public void limpiarLaCasa()
 	{
-		String mensajito="Te hemos limpiado la casa,esta desinfectada";
+		String mensajito="Te hemos limpiado la casa,estas arruinado";
 		System.out.println(mensajito);
 	}
 	
