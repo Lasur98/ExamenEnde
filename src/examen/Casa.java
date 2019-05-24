95b8b1af3fd3ca989c5b87dbfc50aa5cafc7a2d7
Aitor <48829781+Lasur98@users.noreply.github.com>
2019-05-24 13:40:16 +0200

    Creada la clase Casa.java
    Se ha creado con clase casa.java con el metodo limpiarLaCasa() y el
    javadoc

---
+++ src/examen/Casa.java
@@ -0,0 +1,19 @@
+package examen;
+
+public class Casa {
+
+	private float superficie;
+	private String calle;
+	private int codigoPostal;
+	
+	/**
+	 * Un equipo de limpiadores deja la cas alibre de germenes
+	 * y desinfecta todas las superficies
+	 */
+	public void limpiarLaCasa()
+	{
+		String mensajito="Te hemos limpiado la casa,esta desinfectada";
+		System.out.println(mensajito);
+	}
+	
+}
